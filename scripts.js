df75462bd66726ed184389c5a41fb71f9699aeee
Datynford <datynford@gmail.com>
2022-08-14 14:13:43 +0300

    Task #1

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-alert("U were tested!");
+let a = prompt("Введіть кількість чашок");
+if ( a%2 == 0 ){
+    a = a/2;
+}
+else{
+    a = ((a/2) - 1);
+    a = Math.ceil(a);
+}
+console.log(`Кількість бонусних чашок: ${a}`);
