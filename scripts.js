82180ab42cace9838d961123cc99e74f06253b59
Datynford <datynford@gmail.com>
2022-08-16 13:55:39 +0300

    Solving a few tasks

--- scripts.js
+++ scripts.js
@@ -1,9 +1,47 @@
-let a = prompt("Введіть кількість чашок");
-if ( a%2 == 0 ){
-    a = a/2;
-}
-else{
-    a = ((a/2) - 1);
-    a = Math.ceil(a);
-}
-console.log(`Кількість бонусних чашок: ${a}`);
+// let a = prompt("Введіть кількість чашок");
+// b = a/3;
+// b = Math.floor(b);
+// console.log(`Кількість бонусих чашок ${b}`);
+
+// let chicken = prompt("Введіть кількість курей") * 2;
+// let cow = prompt("Введіть кількість корів") * 4;
+// let pig = prompt("Введіть кількість свиней") * 4;
+// let legs = chicken + cow + pig;
+// console.log(`Усього ніг на фермі: ${legs}`);
+
+// let m = prompt('Введіть кількість хвилин');
+// if ( m < 15){
+//     console.log('У першій чверті години');
+// }
+// else if ( m > 15 && m <30){
+//     console.log('У другій чверті години');
+// }
+// else if ( m > 30 && m <45){
+//     console.log('У третій чверті години');
+// }
+// else if ( m > 45 && m < 59){
+//     console.log('У останній чверті години');
+// };
+
+// let stars = prompt('Введіть рядків ')
+// for ( let i = 0; i < stars; i++){
+//     let line = '*'
+//     for(let j = 0; j < i; j++) {
+//         line += '*'
+//       }
+// console.log(`${line}`)
+// };
+
+// let stars = prompt('Введіть кількість рядків');
+// let i = 0;
+// while ( i < stars) {
+//     let j = 0;ІІ
+//     let line = '*'
+//     i++;
+//     while ( j < i-1){
+//         line += '*'
+//         j++;
+//     }
+//     console.log(`${line}`);
+// };
+
